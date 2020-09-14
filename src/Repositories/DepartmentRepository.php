2ace8239fc29c830efd70dae489b672dc0c0a0c3
Duncan McClean <duncan@steadfastcollective.com>
2020-09-14 16:47:11 +0100

    GetDepartmentList

--- src/Repositories/DepartmentRepository.php
+++ src/Repositories/DepartmentRepository.php
@@ -2,12 +2,20 @@
 
 namespace SteadfastCollective\AccountsIQ\Repositories;
 
+use SteadfastCollective\AccountsIQ\AccountsIQ;
 use SteadfastCollective\AccountsIQ\Contracts\DepartmentRepository as ContractsDepartmentRepository;
 
 class DepartmentRepository implements ContractsDepartmentRepository
 {
+    public $accountsIQ;
+
+    public function __construct()
+    {
+        $this->accountsIQ = new AccountsIQ();
+    }
+
     public function getDepartmentList()
     {
-        //
+        return $this->accountsIQ->request('GetDepartmentList');
     }
 }
