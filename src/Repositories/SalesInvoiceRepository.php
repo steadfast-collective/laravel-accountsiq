0bcf0b0aff7966a5e23d88bf0e7d9432f3c006af
Duncan McClean <duncan@steadfastcollective.com>
2020-09-30 12:37:30 +0100

    Throw exception if we hit an error creating a batch sales invoice

--- src/Repositories/SalesInvoiceRepository.php
+++ src/Repositories/SalesInvoiceRepository.php
@@ -4,6 +4,7 @@ namespace SteadfastCollective\AccountsIQ\Repositories;
 
 use SteadfastCollective\AccountsIQ\AccountsIQ;
 use SteadfastCollective\AccountsIQ\Contracts\SalesInvoiceRepository as ContractsSalesInvoiceRepository;
+use SteadfastCollective\AccountsIQ\Exceptions\ResponseException;
 
 class SalesInvoiceRepository implements ContractsSalesInvoiceRepository
 {
@@ -16,6 +17,16 @@ class SalesInvoiceRepository implements ContractsSalesInvoiceRepository
 
     public function createBatchSalesInvoiceBulkGetBackTransactionIDs(array $params)
     {
-        return $this->accountsIQ->request('CreateBatchSalesInvoiceBulkGetBackTransactionIDs', $params);
+        $request = $this->accountsIQ->request('CreateBatchSalesInvoiceBulkGetBackTransactionIDs', $params);
+
+        if (isset($request['CreateBatchSalesInvoiceBulkGetBackTransactionIDsResult']['Result']['WSResultStatus'])) {
+            foreach ($request['CreateBatchSalesInvoiceBulkGetBackTransactionIDsResult']['Result']['WSResultStatus'] as $key => $result) {
+                if ($result['Status'] === 'Failure') {
+                    throw new ResponseException("[$key] ".$result['ErrorCode'].' - '.$result['ErrorMessage']);
+                }
+            }
+        }
+
+        return $request;
     }
 }
