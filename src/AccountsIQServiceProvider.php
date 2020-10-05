895cc5e4bd55e2fab75fd27981daabe5c1d14503
Duncan McClean <duncan@steadfastcollective.com>
2020-10-05 09:38:51 +0100

    Bind 'AccountsIQ' to class
    This will allow the Facade to actually work

--- src/AccountsIQServiceProvider.php
+++ src/AccountsIQServiceProvider.php
@@ -24,5 +24,7 @@ class AccountsIQServiceProvider extends ServiceProvider
     public function register()
     {
         $this->mergeConfigFrom(__DIR__.'/../config/accountsiq.php', 'vesta');
+
+        $this->app->bind('AccountsIQ', AccountsIQ::class);
     }
 }
